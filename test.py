e2b2fe38194b71fbc4629ac11591d3ac97e36452
bugaev-vs <bugaev@gmail.com>
2025-11-12 21:48:16 +0300

    test

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
 print ("hello!")
+app_config = {
+          "host": "127.0.0.1",
+          "port": 5432,
+          "debug": False,
+          "database": {
+              "name": "моя база!",
+              "user": "admin"
+          }
+      }
+print(app_config["database"]["name"])
